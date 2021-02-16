9db4f4b3607d4deb0479c95695f8105f479de6ff
David H Baek <64860355+baekdavid@users.noreply.github.com>
2021-02-16 18:17:05 +0100

    Add files via upload

---
+++ src/main/java/com/example/baekd/baekdApplication.java
@@ -0,0 +1,14 @@
+package com.example.baekd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class baekdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(baekdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baekd/baekdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baekd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class baekdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
